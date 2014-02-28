757dada05d06d2e89186da441361d272589ebbd1
hassan <hassan.reyes@gmail.com>
2014-02-27 22:01:31 -0600

    1. UserName added to User entity. 2. New Web.Infras classes

--- CheckIt.Entities/Entities/User.cs
+++ CheckIt.Entities/Entities/User.cs
@@ -11,6 +11,7 @@ namespace CheckIt.Entities
             this.Answers = new List<Answer>();
         }
 
+        public virtual string UserName { get; set; }
         public virtual string DisplayName { get; set; }
         public virtual string Password { get; set; }
         public virtual string Email { get; set; }
--- CheckIt.Entities/Repostories/IUserRepository.cs
+++ CheckIt.Entities/Repostories/IUserRepository.cs
@@ -4,7 +4,7 @@ namespace CheckIt.Entities
 {
     public interface IUserRepository
     {
-        User GetUserByDisplayName(string displayName);
+        User GetUserByUserName(string userName);
         bool ValidateUser(string id, string password);
         Guid CreateUser(string id, string displayName, string password, string email, int role, string createdBy);
     }
